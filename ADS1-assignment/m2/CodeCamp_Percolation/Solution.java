627cdfc3b525678f42c2c3bb07d01fa92d215bde
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-09-25 17:43:30 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- ADS1-assignment/m2/CodeCamp_Percolation/Solution.java
+++ ADS1-assignment/m2/CodeCamp_Percolation/Solution.java
@@ -9,66 +9,135 @@
 
 
 // You can implement the above API to solve the problem
+/**
+Importing Value.
+*/
 import java.util.Scanner;
+/**
+ * Class for percolation.
+ */
 class Percolation {
-	boolean[][] opened;
-	int top = 0;
-	int bottom;
-	int size;
-	UnionFindPathCompression uf;
-	int count = 0;
-
-	public Percolation(int N) {
-		opened = new boolean[N][N];
-		size = N;
-		bottom = N * N + 1;
-		uf = new UnionFindPathCompression(N * N + 2);
-	}
-
-	public void open(int row, int col) {
-		if (isOpen(row, col)) return;
-		count++;
-		opened[row - 1][col - 1] = true;
-		if (row == 1)
-			uf.union(top, getUFIndex(row, col));
-		if (row == size)
-			uf.union(getUFIndex(row, col), bottom);
-		if (row > 1 && isOpen(row - 1, col))
-			uf.union(getUFIndex(row, col), getUFIndex(row - 1, col));
-		if (row < size && isOpen(row + 1, col))
-			uf.union(getUFIndex(row, col), getUFIndex(row + 1, col));
-		if (col > 1 && isOpen(row, col - 1))
-			uf.union(getUFIndex(row, col), getUFIndex(row, col - 1));
-		if (col < size && isOpen(row, col + 1))
-			uf.union(getUFIndex(row, col), getUFIndex(row, col + 1));
-	}
-
-	public boolean isOpen(int row, int col) {
-		return opened[row - 1][col - 1];
-	}
-
-	public boolean isFull(int row, int col) {
-		return uf.connected(top, getUFIndex(row, col));
-	}
-
-	public boolean percolates() {
-		return uf.connected(top, bottom);
-	}
-
-	private int getUFIndex(int row, int col) {
-		return size * (row - 1) + col;
-	}
+    /**
+     * { Value }.
+     */
+    private boolean[][] opened;
+    /**
+     * { Value }.
+     */
+    private int top = 0;
+    /**
+     * { Value }.
+     */
+    private int bottom;
+    /**
+     * { Value }.
+     */
+    private int size;
+    /**
+     * { Value }.
+     */
+    private UnionFindPathCompression uf;
+    /**
+     * { Value }.
+     */
+    private int count = 0;
+    /**
+     * Constructs the object.
+     *
+     * @param      N     { Integer }.
+     */
+    public Percolation(final int N) {
+        opened = new boolean[N][N];
+        size = N;
+        bottom = N * N + 1;
+        uf = new UnionFindPathCompression(N * N + 2);
+    }
+    /**
+     * { OPen }.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     */
+    public void open(final int row, final int col) {
+        if (isOpen(row, col)) return;
+        count++;
+        opened[row - 1][col - 1] = true;
+        if (row == 1)
+            uf.union(top, getUFIndex(row, col));
+        if (row == size)
+            uf.union(getUFIndex(row, col), bottom);
+        if (row > 1 && isOpen(row - 1, col))
+            uf.union(getUFIndex(row, col), getUFIndex(row - 1, col));
+        if (row < size && isOpen(row + 1, col))
+            uf.union(getUFIndex(row, col), getUFIndex(row + 1, col));
+        if (col > 1 && isOpen(row, col - 1))
+            uf.union(getUFIndex(row, col), getUFIndex(row, col - 1));
+        if (col < size && isOpen(row, col + 1))
+            uf.union(getUFIndex(row, col), getUFIndex(row, col + 1));
+    }
+    /**
+     * Determines if open.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return     True if open, False otherwise.
+     */
+    public boolean isOpen(final int row, final int col) {
+        return opened[row - 1][col - 1];
+    }
+    /**
+     * Determines if full.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return     True if full, False otherwise.
+     */
+    public boolean isFull(final int row, final int col) {
+        return uf.connected(top, getUFIndex(row, col));
+    }
+    /**
+     * { Checks whether Percolates or nOt }.
+     *
+     * @return     { Boolean }.
+     */
+    public boolean percolates() {
+        return uf.connected(top, bottom);
+    }
+    /**
+     * Gets the uf index.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return     The uf index.
+     */
+    private int getUFIndex(final int row, final int col) {
+        return size * (row - 1) + col;
+    }
 }
+/**
+ * Class for solution.
+ */
 class Solution {
-	Solution () {
+    /**
+     * Constructs the object.
+     */
+    private Solution () {
 
-	}
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		Percolation p = new Percolation(scan.nextInt());
-		while (scan.hasNext()) {
-			p.open(scan.nextInt(), scan.nextInt());
-		}
-		System.out.println(p.percolates());
-	}
+    }
+    /**
+     * { Main }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Percolation p = new Percolation(scan.nextInt());
+        while (scan.hasNext()) {
+            p.open(scan.nextInt(), scan.nextInt());
+        }
+        System.out.println(p.percolates());
+    }
 }
