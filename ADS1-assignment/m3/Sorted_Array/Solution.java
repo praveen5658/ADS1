c0686cd668a26e65aaeb84ecb0718d25a6145dc9
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-09-26 22:24:13 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- ADS1-assignment/m3/Sorted_Array/Solution.java
+++ ADS1-assignment/m3/Sorted_Array/Solution.java
@@ -26,13 +26,14 @@ final class Solution {
      */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
-        int n1 = scan.nextInt();
-        int n2 = scan.nextInt();
+        int n1 = Integer.parseInt(scan.nextLine());
+        int n2 = Integer.parseInt(scan.nextLine());
         int[] ar1 = new int[n1];
         int[] ar2 = new int[n2];
         String[] s1 = scan.nextLine().split(",");
         System.out.println(Arrays.toString(s1));
         String[] s2 = scan.nextLine().split(",");
+        System.out.println(Arrays.toString(s2));
         for (int i = 0; i < n1; i++) {
             ar1[i] = Integer.parseInt(s1[i]);
         }
@@ -42,27 +43,26 @@ final class Solution {
         int i = 0;
         int j = 0;
         int k = 0;
-        int[] fin = new int[n1+n2]; 
-        while (k < n1+n2) {
-            if ((ar1[i] < ar2[j]) && (i < n1) && (j < n2)) {
-                fin[k] = ar1[i];
-                i++;
-                k++;
-            }
-            else if ((ar1[i] > ar2[j]) && (i < n1) && (j < n2)) {
-                fin[k] = ar2[j];
-                j++;
-                k++;
-            }
-            else if ((ar1[i] == ar2[j]) && (i < n1) && (j < n2)) {
-                fin[k] = ar2[i];
-                i++;
-                k++;
-                fin[k] = ar2[j];
-                i++;
-                k++;
-            }
-            else {
+        int[] fin = new int[n1 + n2];
+        while (k < n1 + n2) {
+            if ((i < n1) && (j < n2)) {
+                if ((ar1[i] < ar2[j])) {
+                    fin[k] = ar1[i];
+                    i++;
+                    k++;
+                } else if ((ar1[i] > ar2[j])) {
+                    fin[k] = ar2[j];
+                    j++;
+                    k++;
+                } else {
+                    fin[k] = ar2[i];
+                    i++;
+                    k++;
+                    fin[k] = ar2[j];
+                    i++;
+                    k++;
+                }
+            } else {
                 break;
             }
         }
@@ -80,7 +80,7 @@ final class Solution {
                 k++;
             }
         }
-        for (int l = 0;l< fin.length - 1; l++) {
+        for (int l = 0; l < fin.length - 1; l++) {
             System.out.print(fin[l] + ",");
         }
         System.out.print(fin[fin.length - 1]);
