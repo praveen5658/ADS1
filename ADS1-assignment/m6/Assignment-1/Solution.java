df9ae76a03d8920abd8ad838d14e93e4fe1bd56b
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-09-29 11:49:02 +0530

    "check_style_score -> 12 of 12 passed. style score: 0/1 "

--- ADS1-assignment/m6/Assignment-1/Solution.java
+++ ADS1-assignment/m6/Assignment-1/Solution.java
@@ -1,8 +1,23 @@
+/**
+ * { Scanner }.
+ */
 import java.util.Scanner;
+/**
+ * { Arrays }.
+ */
 import java.util.Arrays;
+/**
+ * Class for add large numbers.
+ */
 class AddLargeNumbers {
-
-	public static LinkedList numberToDigits(String number) {
+	/**
+	 * { Numbers to Digits }.
+	 *
+	 * @param      number  The number
+	 *
+	 * @return     { LinkedList }.
+	 */
+	public static LinkedList numberToDigits(final String number) {
 		LinkedList l = new LinkedList();
 		char[] ar = number.toCharArray();
 		for (int i = 0; i < ar.length; i++) {
@@ -10,24 +25,47 @@ class AddLargeNumbers {
 		}
 		return l;
 	}
-
-	public static String digitsToNumberList(LinkedList list) {
+	/**
+	 * { Digits to Numners to List }.
+	 *
+	 * @param      list  The list
+	 *
+	 * @return     {String}.
+	 */
+	public static String digitsToNumberList(final LinkedList list) {
 		String s = "";
 		while (!(list.isEmpty())) {
 			s = Integer.toString(list.popNumber()) + s;
 		}
 		return s;
 	}
-	public static String digitsToNumber(LinkedList list) {
+	/**
+	 * { digits to number }.
+	 *
+	 * @param      list  The list
+	 *
+	 * @return     { String }.
+	 */
+	public static String digitsToNumber(final LinkedList list) {
 		String s = "";
 		while (!(list.isEmpty())) {
 			s += Integer.toString(list.popNumber());
 		}
 		return s;
 	}
-
-	public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
+	/**
+	 * Adds large numbers.
+	 *
+	 * @param      list1  The list 1
+	 * @param      list2  The list 2
+	 *
+	 * @return     { List }.
+	 */
+	public static LinkedList addLargeNumbers(
+	    final LinkedList list1, final LinkedList list2) {
 		int n1, n2, sum;
+		final int ten = 10;
+		final int nine = 9;
 		LinkedList result = new LinkedList();
 		if (list1.size() == list2.size()) {
 			while ((!(list1.isEmpty())) && (!(list2.isEmpty()))) {
@@ -35,10 +73,10 @@ class AddLargeNumbers {
 				n1 = list1.popNumber();
 				n2 = list2.popNumber();
 				sum = n1 + n2;
-				if ((sum > 9) && (list1.size() > 0)) {
+				if ((sum > nine) && (list1.size() > 0)) {
 					list1.head.data += 1;
-					result.addNumber(sum % 10);
-				} else if ((sum > 9) && (list1.size() == 0)) {
+					result.addNumber(sum % ten);
+				} else if ((sum > nine) && (list1.size() == 0)) {
 					result.addNumber(sum);
 				} else {
 					result.addNumber(sum);
@@ -50,17 +88,17 @@ class AddLargeNumbers {
 				n1 = list1.popNumber();
 				n2 = list2.popNumber();
 				sum = n1 + n2;
-				if ((sum > 9) && (list1.size() > 0)) {
+				if ((sum > nine) && (list1.size() > 0)) {
 					list1.head.data += 1;
-					result.addNumber(sum % 10);
-				} else if ((sum > 9) && (list2.size() == 0)) {
-					result.addNumber(sum % 10);
+					result.addNumber(sum % ten);
+				} else if ((sum > nine) && (list2.size() == 0)) {
+					result.addNumber(sum % ten);
 					list1.head.data += 1;
 				} else {
 					result.addNumber(sum);
 				}
 			}
-			while((!(list1.isEmpty()))) {
+			while ((!(list1.isEmpty()))) {
 				n1 = list1.popNumber();
 				result.addNumber(n1);
 			}
@@ -70,17 +108,17 @@ class AddLargeNumbers {
 				n1 = list1.popNumber();
 				n2 = list2.popNumber();
 				sum = n1 + n2;
-				if ((sum > 9) && (list1.size() > 0)) {
+				if ((sum > nine) && (list1.size() > 0)) {
 					list1.head.data += 1;
-					result.addNumber(sum % 10);
-				} else if ((sum > 9) && (list1.size() == 0)) {
-					result.addNumber(sum % 10);
+					result.addNumber(sum % ten);
+				} else if ((sum > nine) && (list1.size() == 0)) {
+					result.addNumber(sum % ten);
 					list2.head.data += 1;
 				} else {
 					result.addNumber(sum);
 				}
 			}
-			while((!(list2.isEmpty()))) {
+			while ((!(list2.isEmpty()))) {
 				n1 = list2.popNumber();
 				result.addNumber(n1);
 			}
@@ -89,9 +127,22 @@ class AddLargeNumbers {
 
 	}
 }
-
+/**
+ * Class for solution.
+ */
 public class Solution {
-	public static void main(String[] args) {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+
+	}
+	/**
+	 * { Main }.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String input = sc.nextLine();
 		String p = sc.nextLine();
@@ -107,10 +158,12 @@ public class Solution {
 		case "addLargeNumbers":
 			pDigits = AddLargeNumbers.numberToDigits(p);
 			qDigits = AddLargeNumbers.numberToDigits(q);
-			LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
+			LinkedList result = AddLargeNumbers.addLargeNumbers(
+			                        pDigits, qDigits);
 			System.out.println(AddLargeNumbers.digitsToNumber(result));
+			break;
+		default :
 			break;
 		}
 	}
-
 }
