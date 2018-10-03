5571216f4ca4afd1409a586d8b20fd6868ee56fa
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-10-03 21:43:39 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ADS1-assignment/m9/9.1 Merge Sort Improvements/Solution.java
+++ ADS1-assignment/m9/9.1 Merge Sort Improvements/Solution.java
@@ -1,23 +1,145 @@
 import java.util.Scanner;
 import java.util.Arrays;
-final class Solution{
-	private Solution(){
+final class Solution {
+	private Solution() {
 
 	}
-	public static void main(final String[] args){
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String[] input;
 		final int fifty = 50;
-		int[] array = new int[fifty];
+		int[] array;
 		int i = 0;
-		while(scan.hasNext()){
-			i = 0;
+		Insertion insertion;
+		MergeSort m;
+		while (scan.hasNext()) {
+			array = new int[fifty];
 			input = scan.nextLine().split(",");
-			for(i = 0; i< input.length;i++){
+			for (i = 0; i < input.length; i++) {
 				array[i] = Integer.parseInt(input[i]);
 			}
 			array = Arrays.copyOf(array, i);
+			// System.out.println(Arrays.toString(array));
+			// insertion = new Insertion();
+			// array = insertion.insertionsort(array);
+			// System.out.println(Arrays.toString(array));
+			m = new MergeSort(array);
+			array = m.mergesort();
 			System.out.println(Arrays.toString(array));
 		}
+	}
+}
+class Insertion {
+	protected Insertion() {
+
+	}
+	public int[] insertionsort(int[] array) {
+		System.out.println("Insertion sort method invoked...");
+		for (int i = 0; i < array.length; i++) {
+			for (int j = i; j > 0; j--) {
+				if (less(array[j], array[j - 1])) {
+					array = swap(array, j, j - 1);
+				} else {
+					break;
+				}
+			}
+		}
+		return array;
+	}
+	public boolean less(int a, int b) {
+		return (a < b);
+	}
+	public int[] swap(int[] array, int j, int k) {
+		int temp = array[k];
+		array[k] = array[j];
+		array[j] = temp;
+		return array;
+	}
+}
+class MergeSort {
+	private int[] array;
+	private int[] aux;
+	Insertion insertion;
+	private int[] sample;
+	protected MergeSort(int[] arr) {
+		array = arr;
+		aux = new int[array.length];
+	}
+	public int[] mergesort() {
+		int low = 0;
+		int high = array.length - 1;
+		sort(array, aux, low, high);
+		return array;
+	}
+	public void sort(int[] array, int[] aux, int low, int high) {
+		if (low >= high) {
+			return;
+		}
+		// System.out.println(Arrays.toString(array));
+		// System.out.println(low);
+		// System.out.println(high);
+		// System.out.println(mid);
+		int mid = low + ((high - low) / 2);
+		if((mid - low + 1)<8){
+			insertion = new Insertion();
+			sample = new int[mid - low + 1];
+			for(int h = 0,l = low; h< sample.length;h++,l++){
+				sample[h] = array[l];
+			}
+			sample = insertion.insertionsort(sample);
+			for(int h = 0,l = low; h< sample.length;h++,l++){
+				array[l] = sample[h];
+			}
+		} else {
+			sort(array, aux, low, mid);
+		}
+		if((high - (mid+1) + 1)<8){
+			insertion = new Insertion();
+			sample = new int[high - (mid+1) + 1];
+			for(int h = 0,l = mid+1; h< sample.length;h++,l++){
+				sample[h] = array[l];
+			}
+			sample = insertion.insertionsort(sample);
+			for(int h = 0,l = mid+1; h< sample.length;h++,l++){
+				array[l] = sample[h];
+			}
+		} else {
+			sort(array, aux, mid + 1, high);
+		}
+		if (array[mid] < array[mid + 1]) {
+			System.out.println("Array is already sorted. So, skipped the call to merge...");
+			return;
+		}
+		// System.out.println(Arrays.toString(array));
+		// System.out.println(low);
+		// System.out.println(high);
+		// System.out.println(mid);
+		merge(array, aux, low, mid, high);
+	}
+	public void merge(int[] array, int[] aux, int low, int mid, int high) {
+		// System.out.println(Arrays.toString(array));
+		for (int k = 0; k <= high; k++) {
+			aux[k] = array[k];
+		}
+		// System.out.println(Arrays.toString(aux));
+		// System.out.println(low);
+		// System.out.println(high);
+		// System.out.println(mid);
+		int i = low, j = mid + 1;
+		for (int k = low; k <= high; k++) {
+			if (i > mid) {
+				array[k] = aux[j++];
+			} else if (j > high) {
+				array[k] = aux[i++];
+			} else if (less(aux[j], aux[i])) {
+				array[k] = aux[j++];
+			} else {
+				array[k] = aux[i++];
+			}
+			// System.out.println(Arrays.toString(array));
+		}
+	}
+	public boolean less(int a, int b) {
+		return (a < b);
 	}
 }
