06451aff3eb2ddd9dd846a7c539d28d9a0da8883
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-10-04 17:15:54 +0530

    "ads1_m10_a1: 1/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m10/10.1 LinkedList using Recursion/Solution.java
+++ ADS1-assignment/m10/10.1 LinkedList using Recursion/Solution.java
@@ -1,66 +1,66 @@
 import java.util.Scanner;
-class Solution{
-	private Solution(){
+class Solution {
+	private Solution() {
 
 	}
-	public static void main(final String[] args){
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String[] input;
 		LinkedList l = new LinkedList();
-		while(scan.hasNext()){
+		while (scan.hasNext()) {
 			input = scan.nextLine().split(" ");
-			switch(input[0]){
-				case "insertAt":
-					try{
+			switch (input[0]) {
+			case "insertAt":
+				try {
 					l.insert(Integer.parseInt(input[1]), Integer.parseInt(input[2]));
 					System.out.println(l);
-					} catch (Exception e){
-						System.out.println(e.getMessage());
-					}
-					break;
-				case "reverse":
-					try{
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+				break;
+			case "reverse":
+				try {
 					l.reverse();
 					System.out.println(l);
-					} catch (Exception e){
-						System.out.println(e.getMessage());
-					}
-					break;
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+				break;
 			}
 		}
 	}
 }
-class LinkedList{
-	protected LinkedList(){
+class LinkedList {
+	protected LinkedList() {
 
 	}
-	class Node{
+	class Node {
 		int data;
 		Node next;
 	}
 	private Node start;
 	private int size = 0;
 	public void insert (int index, int data) throws Exception {
-		if (index < 0 || index > size){
+		if (index < 0 || index > size) {
 			throw new Exception("Can't insert at this position.");
 		}
 		Node node = new Node();
 		int sampleindex = 0;
 		node.data = data;
 		node.next = null;
-		if(start == null){
+		if (start == null) {
 			start = node;
 			size++;
 			return;
 		}
-		if(index == 0){
+		if (index == 0) {
 			node.next = start;
 			start = node;
 			size++;
 			return;
 		}
 		Node samplestart = start;
-		while(sampleindex < index - 1){
+		while (sampleindex < index - 1) {
 			samplestart = samplestart.next;
 			sampleindex++;
 		}
@@ -68,26 +68,30 @@ class LinkedList{
 		samplestart.next = node;
 		size++;
 	}
-	public void reverse() throws Exception{
-		if (size == 0){
+	Node prev = null;
+	Node current = start;
+	Node next = null;
+	public void reverse() throws Exception {
+		if (size == 0) {
 			throw new Exception("No elements to reverse.");
 		}
-		Node prev = null;
-		Node current = start;
-		Node next = null;
-		while(current != null){
-			next = current.next; 
-            current.next = prev; 
-            prev = current; 
-            current = next;
+		if (current != null) {
+			next = current.next;
+			current.next = prev;
+			prev = current;
+			current = next;
+			reverse();
+		}else {
+			start = prev;
+			return;
 		}
-		start = prev;
+		
 	}
-	public String toString(){
+	public String toString() {
 		String s = "";
 		Node sample = start;
-		while(sample != null){
-			s+= Integer.toString(sample.data)+", ";
+		while (sample != null) {
+			s += Integer.toString(sample.data) + ", ";
 			sample = sample.next;
 		}
 		return s.substring(0, s.length() - 2);
