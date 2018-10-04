5ab3d2393e349d67f0e4fd836e8a2d31ae66e915
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-10-04 19:38:51 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m10/10.1 LinkedList using Recursion/Solution.java
+++ ADS1-assignment/m10/10.1 LinkedList using Recursion/Solution.java
@@ -1,97 +1,137 @@
 import java.util.Scanner;
-class Solution {
-	private Solution() {
+/**
+ * { Solution class }.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String[] input;
-		LinkedList l = new LinkedList();;
-		while (scan.hasNext()) {
-			input = scan.nextLine().split(" ");
-			switch (input[0]) {
-			case "insertAt":
-				try {
-					l.insertAt(Integer.parseInt(input[1]), Integer.parseInt(input[2]));
-					System.out.println(l);
-				} catch (Exception e) {
-					System.out.println(e.getMessage());
-				}
-				break;
-			case "reverse":
-				try {
-					l.reverse();
-					System.out.println(l);
-				} catch (Exception e) {
-					System.out.println(e.getMessage());
-				}
-				break;
-			}
-		}
-	}
+    }
+    /**
+     * { Main Method }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] input;
+        LinkedList l = new LinkedList();;
+        while (scan.hasNext()) {
+            input = scan.nextLine().split(" ");
+            switch (input[0]) {
+            case "insertAt":
+                try {
+                    l.insertAt(Integer.parseInt(input[1]), Integer.parseInt(input[2]));
+                    System.out.println(l);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                break;
+            case "reverse":
+                try {
+                    l.reverse();
+                    System.out.println(l);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                break;
+            default:
+                break;
+            }
+        }
+    }
 }
+/**
+ * List of linkeds.
+ */
 class LinkedList {
-	Node start = null;
-	Node test;
-	Node prev;
-	Node temp;
-	int size = 0;
-	class Node {
-		int data;
-		Node next;
-		Node(int data) {
-			this.data = data;
-		}
-	}
-	void insertAt(int index, int inputdata) throws Exception {
-		if (index > size || index < 0) {
-			throw new Exception("Can't insert at this position.");
-			// return;
-		}
-		if (index == 1) {
-			Node node = new Node(inputdata);
-			node.next = test.next;
-			test.next = node;
-			test = start;
-			size++;
-			return;
-		}
-		if (index == 0) {
-			Node node = new Node(inputdata);
-			node.next = test;
-			start = node;
-			test =  start;
-			size++;
-			return;
-		}
-		test = test.next;
-		index--;
-		insertAt(index, inputdata);
-	}
-	void reverse() throws Exception {
-		if (size == 0) {
-			throw new Exception("No elements to reverse.");
-		}
-		if (test.next == null) {
-			start = test;
-			start.next = prev;
-			test = start;
-			prev = null;
-			return;
-		}
-		temp = test;
-		test = test.next;
-		temp.next = prev;
-		prev = temp;
-		reverse();
-	}
-	public String toString() {
-		String s = "";
-		Node sample = start;
-		while (sample != null) {
-			s += Integer.toString(sample.data) + ", ";
-			sample = sample.next;
-		}
-		return s.substring(0, s.length() - 2);
-	}
+    /**
+     * Constructs the object.
+     */
+    protected LinkedList(){
+
+    }
+    private Node start = null;
+    private Node test;
+    private Node prev;
+    private Node temp;
+    private int size = 0;
+    class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+        }
+    }
+    /**
+     * { Insert function }.
+     *
+     * @param      index      The index
+     * @param      inputdata  The inputdata
+     *
+     * @throws     Exception  { Can't Insert at Position }.
+     */
+    void insertAt(int index, int inputdata) throws Exception {
+        if (index > size || index < 0) {
+            throw new Exception("Can't insert at this position.");
+            // return;
+        }
+        if (index == 1) {
+            Node node = new Node(inputdata);
+            node.next = test.next;
+            test.next = node;
+            test = start;
+            size++;
+            return;
+        }
+        if (index == 0) {
+            Node node = new Node(inputdata);
+            node.next = test;
+            start = node;
+            test =  start;
+            size++;
+            return;
+        }
+        test = test.next;
+        index--;
+        insertAt(index, inputdata);
+    }
+    /**
+     * { Reverse of List }.
+     *
+     * @throws     Exception  { Empty List }.
+     */
+    void reverse() throws Exception {
+        if (size == 0) {
+            throw new Exception("No elements to reverse.");
+        }
+        if (test.next == null) {
+            start = test;
+            start.next = prev;
+            test = start;
+            prev = null;
+            return;
+        }
+        temp = test;
+        test = test.next;
+        temp.next = prev;
+        prev = temp;
+        reverse();
+    }
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        String s = "";
+        Node sample = start;
+        while (sample != null) {
+            s += Integer.toString(sample.data) + ", ";
+            sample = sample.next;
+        }
+        return s.substring(0, s.length() - 2);
+    }
 }
