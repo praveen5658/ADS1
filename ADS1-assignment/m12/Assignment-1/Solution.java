b32ed04d8088dfc53b5d784efe253486b7d871b7
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-10-06 12:12:37 +0530

    "ads1_m12_a1: 2/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m12/Assignment-1/Solution.java
+++ ADS1-assignment/m12/Assignment-1/Solution.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
-final class Solution{
-	private Solution(){
+final class Solution {
+	private Solution() {
 
 	}
-	public static void main(final String[] args){
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int studentsnumber = Integer.parseInt(scan.nextLine());
 		int totalvaccancies = Integer.parseInt(scan.nextLine());
-		int unreservedvaccancies= Integer.parseInt(scan.nextLine());
+		int unreservedvaccancies = Integer.parseInt(scan.nextLine());
 		int bcvaccancies = Integer.parseInt(scan.nextLine());
 		int scvaccancies = Integer.parseInt(scan.nextLine());
 		int stvaccancies = Integer.parseInt(scan.nextLine());
 		int i = 0;
 		StudentData[] studentdata = new StudentData[studentsnumber];
-		while(studentsnumber > 0){
+		while (studentsnumber > 0) {
 			studentdata[i] = new StudentData(scan);
 			// System.out.println(studentdata[i]);
 			i++;
@@ -21,28 +21,67 @@ final class Solution{
 		}
 		MeritOrderSort meritordersort = new MeritOrderSort();
 		studentdata = meritordersort.sort(studentdata);
-		for (int k = 0; k < studentdata.length;k++){
+		for (int k = 0; k < studentdata.length; k++) {
 			System.out.println(studentdata[k]);
-		} 
+		}
 		System.out.println();
 		String cat;
 		int h = 0;
 		StudentData[] finaldata = new StudentData[totalvaccancies];
-		for (int k = 0; k < studentdata.length;k++){
+		for (int k = 0; k < studentdata.length; k++) {
 			cat = studentdata[k].getcategory();
-			if (cat.compareTo("ST") == 0){
-				if (stvaccancies > 0){
-					// System.out.println(studentdata[k]);
-					finaldata[h] = studentdata[k];
-					stvaccancies--;
-					totalvaccancies--;
-					h++;
-				} else if (unreservedvaccancies > 0){
-					finaldata[h] = studentdata[k];
-					unreservedvaccancies--;
-					totalvaccancies--;
-					h++;
-				}
+			// if (cat.compareTo("ST") == 0){
+			// 	if (stvaccancies > 0){
+			// 		// System.out.println(studentdata[k]);
+			// 		finaldata[h] = studentdata[k];
+			// 		stvaccancies--;
+			// 		totalvaccancies--;
+			// 		h++;
+			// 	} else if (unreservedvaccancies > 0){
+			// 		finaldata[h] = studentdata[k];
+			// 		unreservedvaccancies--;
+			// 		totalvaccancies--;
+			// 		h++;
+			// 	}
+			// } else if (cat.compareTo("SC") == 0){
+			// 	if (scvaccancies > 0){
+			// 		finaldata[h] = studentdata[k];
+			// 		scvaccancies--;
+			// 		totalvaccancies--;
+			// 		h++;
+			// 	}
+			// 	else if (unreservedvaccancies > 0){
+			// 		finaldata[h] = studentdata[k];
+			// 		unreservedvaccancies--;
+			// 		totalvaccancies--;
+			// 		h++;
+			// 	}
+			// } else if (cat.compareTo("BC") == 0){
+			// 	if (bcvaccancies > 0){
+			// 		finaldata[h] = studentdata[k];
+			// 		bcvaccancies--;
+			// 		totalvaccancies--;
+			// 		h++;
+			// 	}
+			// 	else if (unreservedvaccancies > 0){
+			// 		finaldata[h] = studentdata[k];
+			// 		unreservedvaccancies--;
+			// 		totalvaccancies--;
+			// 		h++;
+			// 	}
+			// } else {
+			// 	if (unreservedvaccancies > 0){
+			// 		finaldata[h] = studentdata[k];
+			// 		unreservedvaccancies--;
+			// 		totalvaccancies--;
+			// 		h++;
+			// 	}
+			// }
+			if (unreservedvaccancies > 0) {
+				finaldata[h] = studentdata[k];
+				unreservedvaccancies--;
+				totalvaccancies--;
+				h++;
 			} else if (cat.compareTo("SC") == 0){
 				if (scvaccancies > 0){
 					finaldata[h] = studentdata[k];
@@ -50,35 +89,24 @@ final class Solution{
 					totalvaccancies--;
 					h++;
 				}
-				else if (unreservedvaccancies > 0){
-					finaldata[h] = studentdata[k];
-					unreservedvaccancies--;
-					totalvaccancies--;
-					h++;
-				}
-			} else if (cat.compareTo("BC") == 0){
+			}else if (cat.compareTo("BC") == 0){
 				if (bcvaccancies > 0){
 					finaldata[h] = studentdata[k];
 					bcvaccancies--;
 					totalvaccancies--;
 					h++;
 				}
-				else if (unreservedvaccancies > 0){
+			} else if (cat.compareTo("ST") == 0){
+				if (stvaccancies > 0){
+					// System.out.println(studentdata[k]);
 					finaldata[h] = studentdata[k];
-					unreservedvaccancies--;
-					totalvaccancies--;
-					h++;
-				}
-			} else {
-				if (unreservedvaccancies > 0){
-					finaldata[h] = studentdata[k];
-					unreservedvaccancies--;
+					stvaccancies--;
 					totalvaccancies--;
 					h++;
 				}
 			}
 		}
-		for (int d = 0; d < finaldata.length;d++){
+		for (int d = 0; d < finaldata.length; d++) {
 			System.out.println(finaldata[d]);
 		}
 	}
