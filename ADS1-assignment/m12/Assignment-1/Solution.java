9d419c74d40a7a3b031249062b485f7c95a945b4
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-10-06 10:50:55 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS1-assignment/m12/Assignment-1/Solution.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+final class Solution{
+	private Solution(){
+
+	}
+	public static void main(final String[] args){
+		Scanner scan = new Scanner(System.in);
+		int studentsnumber = Integer.parseInt(scan.nextLine());
+		int totalvaccancies = Integer.parseInt(scan.nextLine());
+		int unreservedvaccancies= Integer.parseInt(scan.nextLine());
+		int bcvaccancies = Integer.parseInt(scan.nextLine());
+		int scvaccancies = Integer.parseInt(scan.nextLine());
+		int stvaccancies = Integer.parseInt(scan.nextLine());
+		int i = 0;
+		StudentData[] studentdata = new StudentData[studentsnumber];
+		while(studentsnumber > 0){
+			studentdata[i++] = new StudentData(scan);
+			System.out.println(studentdata[i]);
+			studentsnumber--;
+		} 
+	}
+}
---
+++ ADS1-assignment/m12/Assignment-1/StudentData.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+class StudentData {
+	private String name;
+	private String dob;
+	private int subject1marks;
+	private int subject2marks;
+	private int subject3marks;
+	private int totalmarks;
+	private String category;
+	private String[] input;
+	protected StudentData(final Scanner scan) {
+		input = scan.nextLine().split(",");
+		final int two = 2;
+		final int three = 3;
+		final int four = 4;
+		final int five = 5;
+		final int six = 6;
+		name = input[0];
+		dob = input[1];
+		subject1marks = Integer.parseInt(input[two]);
+		subject2marks = Integer.parseInt(input[three]);
+		subject3marks = Integer.parseInt(input[four]);
+		totalmarks = Integer.parseInt(input[five]);
+		category = input[six];
+	}
+	public String getname() {
+		return name;
+	}
+	public String dob() {
+		return dob;
+	}
+	public int getsubject1marks() {
+		return subject1marks;
+	}
+	public int getsubject2marks() {
+		return subject2marks;
+	}
+	public int getsubject3marks() {
+		return subject3marks;
+	}
+	public int gettotalmarks() {
+		return totalmarks;
+	}
+	public String getcategory() {
+		return category;
+	}
+	public String toString() {
+		String s = name + "," + Integer.toString(totalmarks) + "," + category;
+		return s;
+	}
+}
