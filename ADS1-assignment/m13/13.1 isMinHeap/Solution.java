5baede91232392589ba650e39c65bba15f570897
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-10-08 19:36:04 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m13/13.1 isMinHeap/Solution.java
+++ ADS1-assignment/m13/13.1 isMinHeap/Solution.java
@@ -7,116 +7,116 @@ import java.util.Scanner;
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * { Main method }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		// String[] input = scan.nextLine().split(" ");
-		String type = scan.nextLine();
-		String[] input;
-		int testcases = Integer.parseInt(scan.nextLine());
-		int k = 0;
-		switch (type) {
-		case "String":
-			while (testcases > 0) {
-				k = 0;
-				input = scan.nextLine().split(",");
-				if (input[0].equals("")) {
-					System.out.println("false");
-				} else {
-					MinHeap<String> mh = new MinHeap<>(input.length);
-					for (int i = 0; i < input.length; i++) {
-						if (!(mh.insert(input[i]))) {
-							System.out.println("false");
-							k++;
-							break;
-						}
-					}
-					if (k == 0) {
-						System.out.println("true");
-					}
-				}
-				testcases--;
-			}
-			break;
-		case "Integer":
-			while (testcases > 0) {
-				k = 0;
-				input = scan.nextLine().split(",");
-				if (input[0].equals("")) {
-					System.out.println("false");
-				} else {
-					MinHeap<Integer> mh = new MinHeap<>(input.length);
-					for (int i = 0; i < input.length; i++) {
-						if (!(mh.insert(Integer.parseInt(input[i])))) {
-							System.out.println("false");
-							k++;
-							break;
-						}
-					}
-					if (k == 0) {
-						System.out.println("true");
-					}
-				}
-				testcases--;
-			}
-			break;
-		case "Double":
-			while (testcases > 0) {
-				k = 0;
-				input = scan.nextLine().split(",");
-				if (input[0].equals("")) {
-					System.out.println("false");
-				} else {
-					MinHeap<Double> mh = new MinHeap<>(input.length);
-					for (int i = 0; i < input.length; i++) {
-						if (!(mh.insert(Double.parseDouble(input[i])))) {
-							System.out.println("false");
-							k++;
-							break;
-						}
-					}
-					if (k == 0) {
-						System.out.println("true");
-					}
-				}
-				testcases--;
+    }
+    /**
+     * { Main method }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // String[] input = scan.nextLine().split(" ");
+        String type = scan.nextLine();
+        String[] input;
+        int testcases = Integer.parseInt(scan.nextLine());
+        int k = 0;
+        switch (type) {
+        case "String":
+            while (testcases > 0) {
+                k = 0;
+                input = scan.nextLine().split(",");
+                if (input[0].equals("")) {
+                    System.out.println("false");
+                } else {
+                    MinHeap<String> mh = new MinHeap<>(input.length);
+                    for (int i = 0; i < input.length; i++) {
+                        if (!(mh.insert(input[i]))) {
+                            System.out.println("false");
+                            k++;
+                            break;
+                        }
+                    }
+                    if (k == 0) {
+                        System.out.println("true");
+                    }
+                }
+                testcases--;
+            }
+            break;
+        case "Integer":
+            while (testcases > 0) {
+                k = 0;
+                input = scan.nextLine().split(",");
+                if (input[0].equals("")) {
+                    System.out.println("false");
+                } else {
+                    MinHeap<Integer> mh = new MinHeap<>(input.length);
+                    for (int i = 0; i < input.length; i++) {
+                        if (!(mh.insert(Integer.parseInt(input[i])))) {
+                            System.out.println("false");
+                            k++;
+                            break;
+                        }
+                    }
+                    if (k == 0) {
+                        System.out.println("true");
+                    }
+                }
+                testcases--;
+            }
+            break;
+        case "Double":
+            while (testcases > 0) {
+                k = 0;
+                input = scan.nextLine().split(",");
+                if (input[0].equals("")) {
+                    System.out.println("false");
+                } else {
+                    MinHeap<Double> mh = new MinHeap<>(input.length);
+                    for (int i = 0; i < input.length; i++) {
+                        if (!(mh.insert(Double.parseDouble(input[i])))) {
+                            System.out.println("false");
+                            k++;
+                            break;
+                        }
+                    }
+                    if (k == 0) {
+                        System.out.println("true");
+                    }
+                }
+                testcases--;
 
-			}
-			break;
-		case "Float":
-			while (testcases > 0) {
-				k = 0;
-				input = scan.nextLine().split(",");
-				if (input[0].equals("")) {
-					System.out.println("false");
-				} else {
-					MinHeap<Float> mh = new MinHeap<>(input.length);
-					for (int i = 0; i < input.length; i++) {
-						if (!(mh.insert(Float.parseFloat(input[i])))) {
-							System.out.println("false");
-							k++;
-							break;
-						}
-					}
-					if (k == 0) {
-						System.out.println("true");
-					}
-				}
-				testcases--;
-			}
-			break;
-		default:
-			break;
-		}
-	}
+            }
+            break;
+        case "Float":
+            while (testcases > 0) {
+                k = 0;
+                input = scan.nextLine().split(",");
+                if (input[0].equals("")) {
+                    System.out.println("false");
+                } else {
+                    MinHeap<Float> mh = new MinHeap<>(input.length);
+                    for (int i = 0; i < input.length; i++) {
+                        if (!(mh.insert(Float.parseFloat(input[i])))) {
+                            System.out.println("false");
+                            k++;
+                            break;
+                        }
+                    }
+                    if (k == 0) {
+                        System.out.println("true");
+                    }
+                }
+                testcases--;
+            }
+            break;
+        default:
+            break;
+        }
+    }
 }
