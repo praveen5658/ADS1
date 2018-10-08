2b6466b779deec1b065e9e5d6ce9124a9d5dd8fa
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-10-08 19:36:31 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m13/13.1 isMinHeap/MinHeap.java
+++ ADS1-assignment/m13/13.1 isMinHeap/MinHeap.java
@@ -8,61 +8,61 @@ import java.util.Arrays;
  * @param      <E>   { Generic type }.
  */
 class MinHeap<E extends Comparable<E>> {
-	/**
-	 * { Private value }.
-	 */
-	private E[] array;
-	/**
-	 * { Private value }.
-	 */
-	private int size;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      len   The length
-	 */
-	protected MinHeap(final int len) {
-		array = (E[])new Comparable[len + 1];
-		size = 0;
-	}
-	/**
-	 * { Insert method }.
-	 *
-	 * @param      value  The value
-	 *
-	 * @return     { Generic type }.
-	 */
-	public boolean insert(final E value) {
-		array[++size] = value;
-		// System.out.println(size);
-		return (swim(size));
-	}
-	/**
-	 * { For checking minheap }.
-	 *
-	 * @param      k     { Integer }.
-	 *
-	 * @return     { Boolean }.
-	 */
-	public boolean swim(int k) {
-		while (k > 1 && less(k / 2, k)) {
-			// swap(k, k/2);
-			k = k / 2;
-		}
-		if (k > 1) {
-			return false;
-		}
-		return true;
-	}
-	/**
-	 * { For compareTo }.
-	 *
-	 * @param      one   One
-	 * @param      two   Two
-	 *
-	 * @return     { boolean }.
-	 */
-	public boolean less(final int one, final int two) {
-		return ((array[one].compareTo(array[two])) <= 0);
-	}
+    /**
+     * { Private value }.
+     */
+    private E[] array;
+    /**
+     * { Private value }.
+     */
+    private int size;
+    /**
+     * Constructs the object.
+     *
+     * @param      len   The length
+     */
+    protected MinHeap(final int len) {
+        array = (E[])new Comparable[len + 1];
+        size = 0;
+    }
+    /**
+     * { Insert method }.
+     *
+     * @param      value  The value
+     *
+     * @return     { Generic type }.
+     */
+    public boolean insert(final E value) {
+        array[++size] = value;
+        // System.out.println(size);
+        return (swim(size));
+    }
+    /**
+     * { For checking minheap }.
+     *
+     * @param      k     { Integer }.
+     *
+     * @return     { Boolean }.
+     */
+    public boolean swim(int k) {
+        while (k > 1 && less(k / 2, k)) {
+            // swap(k, k/2);
+            k = k / 2;
+        }
+        if (k > 1) {
+            return false;
+        }
+        return true;
+    }
+    /**
+     * { For compareTo }.
+     *
+     * @param      one   One
+     * @param      two   Two
+     *
+     * @return     { boolean }.
+     */
+    public boolean less(final int one, final int two) {
+        return ((array[one].compareTo(array[two])) <= 0);
+    }
 }
