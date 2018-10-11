16e6fc47bfeffdec3d05d610fa5bba686c8ddcc6
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-10-11 20:18:19 +0530

    "ads1_m16_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m16/16.1 BST/Solution.java
+++ ADS1-assignment/m16/16.1 BST/Solution.java
@@ -6,38 +6,40 @@ import java.util.Scanner;
  * { Solution class }.
  */
 public final class Solution {
-	/**.
-	 * Constructs the object.
-	 */
-	private Solution() {
-	}
-	/**.
-	 * { Main method }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		BinarySearchTree bst = new BinarySearchTree();
-		while (sc.hasNext()) {
-			String input = sc.nextLine();
-			String[] tokens = input.split(",");
-			switch (tokens[0]) {
-			case "put":
-				bst.put(new BookList(tokens[1], tokens[2],
-				                     Float.parseFloat(tokens[2 + 1])),
-				        Integer.parseInt(tokens[2 + 2]));
-				break;
-			case "get":
-				System.out.println(bst.get(
-				                       new BookList(tokens[1],
-				                                    tokens[2]
-				                                    , Float.parseFloat(
-				                                        tokens[2 + 1]))));
-				break;
-			default:
-				break;
-			}
-		}
-	}
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**.
+     * { Main method }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BinarySearchTree bst = new BinarySearchTree();
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            String[] tokens = input.split(",");
+            switch (tokens[0]) {
+            case "put":
+                bst.put(new BookList(
+                            tokens[1], tokens[2],
+                            Float.parseFloat(tokens[2 + 1])),
+                        Integer.parseInt(tokens[2 + 2]));
+                break;
+            case "get":
+                System.out.println(
+                    bst.get(
+                        new BookList(tokens[1],
+                                     tokens[2]
+                                     ,Float.parseFloat(
+                                         tokens[2 + 1]))));
+                break;
+            default:
+                break;
+            }
+        }
+    }
 }
