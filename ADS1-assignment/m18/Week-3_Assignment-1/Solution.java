fd999c7a807ba3dc12732f06a4850c4ca5e04080
praveen5658 <praveennalamolu1997@msitprogram.net>
2018-10-13 11:45:38 +0530

    "ads1_week3_exam: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1-assignment/m18/Week-3_Assignment-1/Solution.java
+++ ADS1-assignment/m18/Week-3_Assignment-1/Solution.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
-final class Solution{
-	private Solution(){
+final class Solution {
+	private Solution() {
 
 	}
-	public static void main(final String[] args){
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int noofstocksperhour = Integer.parseInt(scan.nextLine());
 		int n = 6;
@@ -18,12 +18,12 @@ final class Solution{
 		int s = 0;
 		BinarySearchST bstmin = new BinarySearchST();
 		BinarySearchST bstmax = new BinarySearchST();
-		while(n>0){
+		while (n > 0) {
 			minpq = new MinPQ(5);
 			maxpq = new MaxPQ(5);
 			keys = new String[noofstocksperhour];
 			values = new float[noofstocksperhour];
-			for(int i = 0; i< noofstocksperhour;i++){
+			for (int i = 0; i < noofstocksperhour; i++) {
 				input = scan.nextLine().split(",");
 				key = input[0];
 				keys[i] = input[0];
@@ -32,38 +32,50 @@ final class Solution{
 				minpq.insert(value);
 				maxpq.insert(value);
 			}
-			for(int m = 0; m<5;m++){
+			for (int m = 0; m < 5; m++) {
 				f = (float)maxpq.delMax();
-				for(s = 0; s < noofstocksperhour;s++){
-					if(values[s]==f){
+				for (s = 0; s < noofstocksperhour; s++) {
+					if (values[s] == f &&  values[s+1] == f) {
+						key = keys[s];
+						value = f;
+						if (bstmax.contains(key)) {
+							// bstmax.put(key, bstmax.get(key)+1);
+						} else {
+							bstmax.put(key, 1);
+						}
+						System.out.print(key + " " + Float.toString(value) + "\n");
+						s++;
+						m++;
+						break;
+					} else if (values[s] == f){
 						break;
 					}
 				}
 				key = keys[s];
 				value = f;
-				if (bstmax.contains(key)){
+				if (bstmax.contains(key)) {
 					// bstmax.put(key, bstmax.get(key)+1);
-				}else {
+				} else {
 					bstmax.put(key, 1);
 				}
-				System.out.print(key + " "+ Float.toString(value) + "\n");
+				System.out.print(key + " " + Float.toString(value) + "\n");
 			}
 			System.out.println();
-			for(int m = 0; m<5;m++){
+			for (int m = 0; m < 5; m++) {
 				f = (float)minpq.delMin();
-				for(s = 0; s < noofstocksperhour;s++){
-					if(values[s]==f){
+				for (s = 0; s < noofstocksperhour; s++) {
+					if (values[s] == f) {
 						break;
 					}
 				}
 				key = keys[s];
 				value = f;
-				if (bstmin.contains(key)){
+				if (bstmin.contains(key)) {
 					// bstmin.put(key, bstmin.get(key)+(float)1);
-				}else {
+				} else {
 					bstmin.put(key, 1);
 				}
-				System.out.print(key + " "+ Float.toString(value) + "\n");
+				System.out.print(key + " " + Float.toString(value) + "\n");
 			}
 			System.out.println();
 			n--;
